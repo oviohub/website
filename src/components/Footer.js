ea7d5abdebceeee09f4e6e3ed53b65eadcbd89c4
Mamadou Ouologuem <mamadou.ouologuem02@gmail.com>
2019-08-04 00:34:00 +0200

    About Pages: Our partners

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { Grid, Typography, Icon } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
-import menuItems from '../services/MenuItems';
-import { getPageUrl } from '../Routes';
-import { useStylesBase } from './styledComponents';
+import { footerMenu } from '../services/MenuItems';
+import { spacing, useStylesBase } from './styledComponents';
 import logo from '../assets/logo/logo.orange.svg';
 import iconFacebook from '../assets/icons/icon.facebook.orange.svg';
 import iconLinkedIn from '../assets/icons/icon.linkedin.orange.svg';
@@ -12,37 +13,67 @@ import iconTwitter from '../assets/icons/icon.twitter.orange.svg';
 const socialMedias = [
   {
     image: { src: iconFacebook, alt: 'Link to Facebook' },
-    link: getPageUrl('HomePage'),
+    link: 'https://www.facebook.com/oviohub',
   },
   {
     image: { src: iconLinkedIn, alt: 'Link to LinkedIn' },
-    link: getPageUrl('HomePage'),
+    link: 'https://www.linkedin.com/company/oviohub/',
   },
   {
     image: { src: iconTwitter, alt: 'Link to Twitter' },
-    link: getPageUrl('HomePage'),
+    link: 'https://twitter.com/OvioHub',
   },
 ];
 
+const useStyles = makeStyles({
+  lightIcon: { opacity: 0.6 },
+  itemContainer: {
+    width: 'auto',
+  },
+  itemText: {
+    opacity: 1,
+    fontWeight: 'bold',
+    marginBottom: spacing(4),
+  },
+  links: { textDecoration: 'none', marginBottom: spacing(4) },
+});
+
 const Footer = () => {
-  const { muiGridBlockContainer, muiIconLogo, muiTypographyOverlineLight } = useStylesBase();
+  const { muiGridBlockContainer, muiIconLogo } = useStylesBase();
+  const { lightIcon, itemContainer, itemText, links } = useStyles();
   return (
     <Grid className={muiGridBlockContainer} container justify="flex-end">
-      <Grid item container sm={6} direction="column">
-        <Icon className={muiIconLogo} component="img" src={logo} alt="Ovio" />
+      <Grid item container xs={6} direction="column">
+        <img className={muiIconLogo} component="img" src={logo} alt="Ovio" />
         <Grid>
           {socialMedias.map(item => (
-            <Icon component="img" key={item.image.alt} src={item.image.src} alt={item.image.alt} />
+            <a key={item.link} href={item.link} target="_blank" rel="noreferrer noopener">
+              <Icon component="img" className={lightIcon} src={item.image.src} alt={item.image.alt} />
+            </a>
           ))}
         </Grid>
         <Typography variant="caption">© 2019 Ovio Inc - All Rights Reserved</Typography>
       </Grid>
-      <Grid item container sm={6}>
-        {menuItems.filter(item => item.label !== 'About').map(item => (
-          <Grid container direction="column" item key={item.label} style={{ width: 'auto' }}>
-            <Typography variant="overline">{item.label}</Typography>
-            <Typography variant="overline" className={muiTypographyOverlineLight}>{item.label}</Typography>
-            <Typography variant="overline" className={muiTypographyOverlineLight}>{item.label}</Typography>
+      <Grid item container xs={6} justify="space-between">
+        {footerMenu.map(item => (
+          <Grid key={item.label} className={itemContainer} container direction="column">
+            <Typography className={itemText} variant="caption">{item.label}</Typography>
+            {item.subMenu.map(subItem => (
+              <React.Fragment key={subItem.label}>
+                {subItem.link && (
+                  <Link to={subItem.link} className={links}>
+                    <Typography variant="caption">{subItem.label}</Typography>
+                  </Link>
+                )}
+                {subItem.href ? (
+                  <a href={subItem.href} className={links} target="_blank" rel="noreferrer noopener">
+                    <Typography variant="caption">{subItem.label}</Typography>
+                  </a>
+                )
+                  : !subItem.link && <Typography variant="caption">{subItem.label}</Typography>
+                }
+              </React.Fragment>
+            ))}
           </Grid>
         ))}
       </Grid>
--- src/components/ui-library/LineSeparator.js
+++ src/components/ui-library/LineSeparator.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { spacing, useStylesBase, ThickLine } from '../styledComponents';
+import { spacing, useStylesBase, colors } from '../styledComponents';
 
+const lineHeight = spacing(1.5);
 const useStyles = makeStyles({
-  container: { marginBottom: spacing(12) },
+  container: {
+    marginBottom: spacing(12),
+    height: lineHeight,
+  },
   lineContainer: {
     position: 'absolute',
     right: '0px',
+  },
+  thickLine: {
+    width: '250px',
+    height: lineHeight,
+    backgroundColor: colors.orange,
   },
 });
 
 const LineSeparator = () => {
   const { muiGridBlockContainer } = useStylesBase();
-  const { container, lineContainer } = useStyles();
+  const { container, lineContainer, thickLine } = useStyles();
   return (
-    <Grid container justify="flex-end" className={`${muiGridBlockContainer} ${container}`}>
-      <Grid className={lineContainer}><ThickLine width="250px" /></Grid>
+    <Grid container justify="flex-end" className={`${muiGridBlockContainer} ${container} ${container}`}>
+      <Grid className={lineContainer}>
+        <div className={thickLine} width="250px" />
+      </Grid>
     </Grid>
   );
 };
