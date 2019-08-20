2563fa3a5b22bc2ee0b3a9da139a9b0a76934c0e
Mamadou Ouologuem <40799514+mamadOuologuem@users.noreply.github.com>
2019-08-20 13:37:35 -0700

    🎉  Model page: mobile version (#17)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,19 +6,21 @@ import { makeStyles } from '@material-ui/styles';
 import { menuHeightOffset, spacing } from './styledComponents';
 import { toFormattedText } from '../services/formatting';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   mainContainer: { paddingTop: `${menuHeightOffset}px` },
   heroContainer: ({ backgroundImage }) => ({
     width: 'calc(100vw - 15px)', // 15px == scroll bar width
     height: '500px', // hero height
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'cover',
+    [theme.breakpoints.down('md')]: { width: '100vw' },
+
   }),
   subsubtitle1: {
     marginTop: spacing(3),
     textAlign: 'center',
   },
-});
+}));
 
 const Hero = ({ title, subtitle, subSubtitle, backgroundImage }) => {
   const { mainContainer, heroContainer, subsubtitle1 } = useStyles({ backgroundImage });
@@ -26,7 +28,7 @@ const Hero = ({ title, subtitle, subSubtitle, backgroundImage }) => {
     <Grid className={mainContainer} container justify="center">
       <Grid>
         <Grid className={heroContainer} container item justify="center" alignItems="center">
-          <Grid container item direction="column" justify="center" alignItems="center" xs={8}>
+          <Grid container item direction="column" justify="center" alignItems="center" xs={10} sm={8}>
             <Typography variant="h1">{title}</Typography>
             <Typography variant="subtitle1">{toFormattedText(subtitle)}</Typography>
             {subSubtitle && (
